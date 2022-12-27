5b4e0d5797f1f024107bc8f019f1cb92095e0dee
Akbar <akbarbadmus07@gmail.com>
2022-12-27 16:46:30 +0100

    px-market

--- pages/Marketplace/index.js
+++ pages/Marketplace/index.js
@@ -12,7 +12,7 @@ const Marketplace = ({ product }) => {
   const [ByYear, setByYear] = useState(false);
 
   return (
-    <div className="flex flex-col justify-center mt-20 ">
+    <div className="flex flex-col justify-center mt-20 px-8 lg:px-16">
       <div className="flex justify-center items-center">
         <input
           placeholder="search"
